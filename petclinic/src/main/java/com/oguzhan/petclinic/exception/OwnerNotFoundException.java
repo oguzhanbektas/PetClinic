f62e67fbde6e758c57d3eec585a0542b2c4b1939
Oğuzhan BEKTAŞ <ozibektas@gmail.com>
2021-02-21 12:30:28 +0300

    service and exception classes are added.

---
+++ petclinic/src/main/java/com/oguzhan/petclinic/exception/OwnerNotFoundException.java
@@ -0,0 +1,8 @@
+package com.oguzhan.petclinic.exception;
+
+public class OwnerNotFoundException extends Exception {
+
+    public OwnerNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ petclinic/src/main/java/com/oguzhan/petclinic/service/PetClinicService.java
@@ -0,0 +1,21 @@
+package com.oguzhan.petclinic.service;
+
+import com.oguzhan.petclinic.exception.OwnerNotFoundException;
+import com.oguzhan.petclinic.model.Owner;
+
+import java.util.List;
+
+public interface PetClinicService {
+
+    List<Owner> findOwners();
+
+    List<Owner> findOwners(String lasName);
+
+    Owner findOwner(Long id) throws OwnerNotFoundException;
+
+    void createOwner(Owner owner);
+
+    void updateOwner(Owner owner);
+
+    void deleteOwner(Long id);
+}
