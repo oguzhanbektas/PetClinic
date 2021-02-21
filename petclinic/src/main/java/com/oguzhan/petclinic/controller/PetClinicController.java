f8c07fd973abf7cb8254548a0e70add3b91c482f
Oğuzhan BEKTAŞ <ozibektas@gmail.com>
2021-02-21 14:24:11 +0300

    project package change is war in pom.xml suffix and prefix are added in application properties owner page is created. owner method is created

--- petclinic/src/main/java/com/oguzhan/petclinic/controller/PetClinicController.java
+++ petclinic/src/main/java/com/oguzhan/petclinic/controller/PetClinicController.java
@@ -1,11 +1,29 @@
 package com.oguzhan.petclinic.controller;
 
+import com.oguzhan.petclinic.service.PetClinicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PetClinicController {
+
+    private PetClinicService petClinicService;
+
+    @Autowired
+    public PetClinicController(PetClinicService petClinicService) {
+        this.petClinicService = petClinicService;
+    }
+
+    @RequestMapping("/owners")
+    public ModelAndView getOwners(){
+        ModelAndView mav=new ModelAndView();
+        mav.addObject("owners",petClinicService.findOwners());
+        mav.setViewName("owners");
+        return mav;
+    }
 
     @RequestMapping("/pcs")
     @ResponseBody
---
+++ petclinic/src/main/webapp/WEB-INF/jsp/owners.jsp
@@ -0,0 +1,32 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bforb
+  Date: 21.02.2021
+  Time: 14:13
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<table>
+    <thead>
+    <tr style="font-weight: bold; background-color: aqua">
+        <td>Id</td>
+        <td>First Name</td>
+        <td>Last Name</td>
+    </tr>
+    </thead>
+    <c:forEach items="${owners}" var="owner" varStatus="status">
+        <tr bgcolor="${status.index % 2 ==0 ? 'white' : 'lightgray'}">
+            <td>${owner.id}</td>
+            <td>${owner.firstName}</td>
+            <td>${owner.lastName}</td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
