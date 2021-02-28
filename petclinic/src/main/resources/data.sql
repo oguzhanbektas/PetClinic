53737fa941bf5801413ae2329d60f8a4ff954103
Oğuzhan BEKTAŞ <ozibektas@gmail.com>
2021-02-28 22:34:29 +0300

    Test schema is created.

---
+++ petclinic/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+insert into t_owner(id, first_name, last_name) values (1, 'Oğuzhan', 'BEKTAŞ');
+insert into t_owner(id, first_name, last_name) values (2, 'Dilara', 'BEKTAŞ');
+insert into t_owner(id, first_name, last_name) values (3, 'Muharrem', 'ESA');
+insert into t_owner(id, first_name, last_name) values (4, 'Aykut', 'Önal');
+insert into t_owner(id, first_name, last_name) values (5, 'Eray', 'Demir');
+insert into t_owner(id, first_name, last_name) values (6, 'Oğuz', 'Zarco');
+insert into t_owner(id, first_name, last_name) values (7, 'Zalim', 'Dünya');
+insert into t_owner(id, first_name, last_name) values (8, 'Birgüm', 'Yüzümüze');
+insert into t_owner(id, first_name, last_name) values (9, 'Gülermi', 'Be');
+insert into t_owner(id, first_name, last_name) values (10, 'Ne', 'Dersin');
+
+
+
+insert into t_pet(id, name, birth_date, owner_id) values (1,'Bulut','2020-02-02',1);
+insert into t_pet(id, name, birth_date, owner_id) values (2,'Cicikuş','2019-01-02',1);
+insert into t_pet(id, name, birth_date, owner_id) values (3,'Kontra','2018-02-04',2);
+insert into t_pet(id, name, birth_date, owner_id) values (4,'Ziya','2015-02-02',3);
+insert into t_pet(id, name, birth_date, owner_id) values (5,'Cenkutay','2000-02-02',4);
+insert into t_pet(id, name, birth_date, owner_id) values (6,'Limon','2003-02-02',5);
+insert into t_pet(id, name, birth_date, owner_id) values (7,'Paşa','2009-02-02',6);
+insert into t_pet(id, name, birth_date, owner_id) values (8,'Kasap','2008-02-02',7);
+insert into t_pet(id, name, birth_date, owner_id) values (10,'Raif','20004-02-02',8);
+insert into t_pet(id, name, birth_date, owner_id) values (11,'Calculues','2020-02-02',9);
+insert into t_pet(id, name, birth_date, owner_id) values (12,'Duru','2019-02-02',10);
+insert into t_pet(id, name, birth_date, owner_id) values (13,'Yaş','2018-02-02',8);
+insert into t_pet(id, name, birth_date, owner_id) values (14,'Sarı','2017-02-02',4);
+insert into t_pet(id, name, birth_date, owner_id) values (15,'Pammuk','2016-02-02',2);
